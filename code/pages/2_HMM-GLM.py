cf2751175f2d099dc3cf30296417934c9a1b0575
houhan@gmail.com <han.hou@alleninstitute.org>
2024-07-06 04:16:20 +0000

    feature: improve layout

--- code/pages/2_HMM-GLM.py
+++ code/pages/2_HMM-GLM.py
@@ -1,4 +1,6 @@
 """Visualize HMM-GLM results from Faeze
+
+Han, ChatGPT
 """
 import os
 from PIL import Image
@@ -15,17 +17,12 @@ s3 = s3fs.S3FileSystem(anon=False)
 
 # Function to open image from S3
 @st.cache_data(ttl=24*3600)
-def open_image_from_s3(file_key):
+def open_image_from_s3(file_key, crop=None, **kwargs):
     with s3.open(f'{file_key}', 'rb') as f:
         img = Image.open(f)
-        img = img.crop()
-        return img
-
-# Function to get image file names in a folder
-def get_image_files(bucket_name, prefix):
-    all_objects = s3.ls(f'{bucket_name}/{prefix}', detail=True)
-    image_files = [obj['Key'] for obj in all_objects if obj['type'] == 'file' and obj['Key'].endswith('.png')]
-    return image_files
+        img = img.crop(crop)
+        
+    st.image(img, **kwargs)
 
 # Get the list of data folders
 data_folders = [os.path.basename(f) for f in 
@@ -36,6 +33,7 @@ data_folders = [os.path.basename(f) for f in
 with st.sidebar:
     widget_data_folder = st.container()
     widget_mouse = st.container()
+    widget_n_states = st.container()
     widget_model_comparison = st.container()
     
 data_folder_selected = widget_data_folder.selectbox('Select Data Folder', data_folders)
@@ -51,40 +49,56 @@ if data_folder_selected:
 
     # Show mouse-wise figures
     if mouse_selected:
+        mouse_folder = f'{bucket_name}/{data_folder_selected}/{mouse_selected}'
         
         with widget_model_comparison:
-            mouse_prefix = f'{data_folder_selected}/{mouse_selected}/'
-            figs_model_comparison = ['AIC.png', 'BIC.png', 'LL.png']
-            figs_model_comparison_keys = [f'{mouse_prefix}{fig}' for fig in figs_model_comparison]
+            fig_model_comparisons = ['AIC.png', 'BIC.png', 'LL.png']
 
-            for i, img_key in enumerate(figs_model_comparison_keys):
-                img = open_image_from_s3(f'{bucket_name}/{img_key}')
-                st.image(img, caption=os.path.basename(img_key))
+            for i, fig_model_comparison in enumerate(fig_model_comparisons):
+                img = open_image_from_s3(
+                    f'{mouse_folder}/{fig_model_comparison}',
+                    caption=fig_model_comparison,
+                    )
 
         # Number of states selection
-        num_states = st.columns([1, 5])[0].selectbox('Select Number of States', 
-                                                    ['two_states', 'three_states', 'four_states'],
-                                                    index=2, # Default shows four states
-                                                    )
+        num_states = widget_n_states.selectbox('Select Number of States', 
+                                                ['two_states', 'three_states', 'four_states'],
+                                                index=2, # Default shows four states
+                                                )
 
         if num_states:
-            states_prefix = f'{mouse_prefix}{num_states}/'
-            figs_states = ['GLM_Weights.png', 'GLM_TM.png', 'frac_occupancy.png', 'frac_occupancy_of_sessions.png']
-            figs_states_keys = [f'{states_prefix}{img}' for img in figs_states]
+            num_states_folder = f'{mouse_folder}/{num_states}'
+            fig_states = ['GLM_Weights.png', 'GLM_TM.png', 'frac_occupancy.png', 'frac_occupancy_of_sessions.png']
 
-            cols = st.columns(len(figs_states_keys))
-            for i, img_key in enumerate(figs_states_keys):
-                with cols[i]:
-                    img = open_image_from_s3(f'{bucket_name}/{img_key}')
-                    st.image(img, caption=os.path.basename(img_key))
-
+            cols = st.columns([1, 0.2, 1])
+            with cols[0]:
+                open_image_from_s3(f'{num_states_folder}/GLM_Weights.png', 
+                                   caption='GLM weights')
+            with cols[2]:
+                open_image_from_s3(f'{num_states_folder}/frac_occupancy_of_sessions.png', 
+                                   caption='Fraction occupancy over sessions')
+            with cols[1]:
+                open_image_from_s3(f'{num_states_folder}/frac_occupancy.png', 
+                                   caption='Fraction occupancy (all)')
+                open_image_from_s3(f'{num_states_folder}/GLM_TM.png', 
+                                   caption='Fraction occupancy (all)')    
+                                            
             # Grouped by selection
-            grouped_by = st.selectbox('Grouped By', ['grouped_by_sessions', 'grouped_by_sessions_conventional_view', 'grouped_by_states'])
-
-            if grouped_by:
-                grouped_prefix = f'{states_prefix}{grouped_by}/'
-                figs_session_keys = get_image_files(bucket_name, grouped_prefix)
+            with st.container(height=2000):
+                cols = st.columns([1.5, 1, 4])
+                grouped_by = cols[0].selectbox('Grouped By', ['grouped_by_sessions', 'grouped_by_sessions_conventional_view', 'grouped_by_states'])
+                num_cols = cols[1].number_input(
+                    label='number of columns',
+                    min_value=1,
+                    max_value=10,
+                    value=2,
+                )
                 
-                for img_key in figs_session_keys:
-                    img = open_image_from_s3(img_key)
-                    st.image(img, caption=os.path.basename(img_key))
+                if grouped_by:
+                    fig_sessions = s3.glob(f'{num_states_folder}/{grouped_by}/*.png')
+                    cols = st.columns(num_cols)
+                    
+                    for i, fig_session in enumerate(fig_sessions):
+                        with cols[i % num_cols]:
+                            img = open_image_from_s3(fig_session, caption=fig_session)
+
