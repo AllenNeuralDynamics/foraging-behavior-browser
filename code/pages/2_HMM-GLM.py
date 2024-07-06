eed03495e40a3e19f75b8b1a14ab94d9c46c92b1
houhan@gmail.com <han.hou@alleninstitute.org>
2024-07-06 03:16:22 +0000

    feat: put model comparison on sidebar

--- code/pages/2_HMM-GLM.py
+++ code/pages/2_HMM-GLM.py
@@ -14,7 +14,7 @@ bucket_name = "s3://aind-behavior-data/faeze/HMM-GLM"
 s3 = s3fs.S3FileSystem(anon=False)
 
 # Function to open image from S3
-@st.cache_data(ttl=24*3600, max_entries=20)
+@st.cache_data(ttl=24*3600)
 def open_image_from_s3(file_key):
     with s3.open(f'{file_key}', 'rb') as f:
         img = Image.open(f)
@@ -33,40 +33,47 @@ data_folders = [os.path.basename(f) for f in
                 ]
 
 # Data folder selection dropdown
-data_folder_selected = st.selectbox('Select Data Folder', data_folders)
+with st.sidebar:
+    widget_data_folder = st.container()
+    widget_mouse = st.container()
+    widget_model_comparison = st.container()
+    
+data_folder_selected = widget_data_folder.selectbox('Select Data Folder', data_folders)
 
 if data_folder_selected:
-
     # Get the list of mice folders
     mice = [os.path.basename(f) for f in
                      s3.glob(f'{bucket_name}/{data_folder_selected}/*')
                      if not os.path.basename(f).startswith('.')
                     ]
     # Mouse selection dropdown
-    mouse_selected = st.selectbox('Select Mouse', mice)
+    mouse_selected = widget_mouse.selectbox('Select Mouse', mice)
 
     # Show mouse-wise figures
     if mouse_selected:
-        mouse_prefix = f'{data_folder_selected}/{mouse_selected}/'
-        mouse_figures = ['AIC.png', 'BIC.png', 'LL.png']
-        mouse_figures_keys = [f'{mouse_prefix}{fig}' for fig in mouse_figures]
+        
+        with widget_model_comparison:
+            mouse_prefix = f'{data_folder_selected}/{mouse_selected}/'
+            figs_model_comparison = ['AIC.png', 'BIC.png', 'LL.png']
+            figs_model_comparison_keys = [f'{mouse_prefix}{fig}' for fig in figs_model_comparison]
 
-        cols = st.columns(len(mouse_figures_keys))
-        for i, img_key in enumerate(mouse_figures_keys):
-            with cols[i]:
+            for i, img_key in enumerate(figs_model_comparison_keys):
                 img = open_image_from_s3(f'{bucket_name}/{img_key}')
                 st.image(img, caption=os.path.basename(img_key))
 
         # Number of states selection
-        num_states = st.selectbox('Select Number of States', ['two_states', 'three_states', 'four_states'])
+        num_states = st.columns([1, 5])[0].selectbox('Select Number of States', 
+                                                    ['two_states', 'three_states', 'four_states'],
+                                                    index=2, # Default shows four states
+                                                    )
 
         if num_states:
             states_prefix = f'{mouse_prefix}{num_states}/'
-            glm_images = ['GLM_Weights.png', 'GLM_TM.png', 'frac_occupancy.png', 'frac_occupancy_of_sessions.png']
-            glm_images_keys = [f'{states_prefix}{img}' for img in glm_images]
+            figs_states = ['GLM_Weights.png', 'GLM_TM.png', 'frac_occupancy.png', 'frac_occupancy_of_sessions.png']
+            figs_states_keys = [f'{states_prefix}{img}' for img in figs_states]
 
-            cols = st.columns(len(glm_images_keys))
-            for i, img_key in enumerate(glm_images_keys):
+            cols = st.columns(len(figs_states_keys))
+            for i, img_key in enumerate(figs_states_keys):
                 with cols[i]:
                     img = open_image_from_s3(f'{bucket_name}/{img_key}')
                     st.image(img, caption=os.path.basename(img_key))
@@ -76,8 +83,8 @@ if data_folder_selected:
 
             if grouped_by:
                 grouped_prefix = f'{states_prefix}{grouped_by}/'
-                session_images_keys = get_image_files(bucket_name, grouped_prefix)
+                figs_session_keys = get_image_files(bucket_name, grouped_prefix)
                 
-                for img_key in session_images_keys:
+                for img_key in figs_session_keys:
                     img = open_image_from_s3(img_key)
                     st.image(img, caption=os.path.basename(img_key))
