540e03043bd775d52225213e64da5889d993721e
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-31 01:27:01 +0000

    feat: add params selectors

--- code/pages/4_RL model playground.py
+++ code/pages/4_RL model playground.py
@@ -9,6 +9,7 @@ import inspect
 from aind_behavior_gym.dynamic_foraging.task import CoupledBlockTask
 from aind_dynamic_foraging_models import generative_model
 from aind_dynamic_foraging_models.generative_model import ForagerCollection
+from aind_dynamic_foraging_models.generative_model.params import ParamsSymbols
 
 try:
     st.set_page_config(layout="wide", 
@@ -27,7 +28,12 @@ model_families = {
     "Loss counting": "ForagerLossCounting"
 }
 
-def _get_agent_args(agent_class):
+para_range_override = {
+    "biasL": [-5.0, 5.0],
+    "choice_kernel_relative_weight": [0.0, 2.0],
+}
+
+def _get_agent_args_options(agent_class):
     type_hints = get_type_hints(agent_class.__init__)
     signature = inspect.signature(agent_class.__init__)
 
@@ -41,7 +47,38 @@ def _get_agent_args(agent_class):
             agent_args_options[arg] = {'options': literal_values, 'default': default_value}
     return agent_args_options
 
-def get_arg_params(agent_args_options):
+
+def _get_params_options(param_model):
+    # Get the schema
+    params_schema = param_model.model_json_schema()["properties"]
+
+    # Extract ge and le constraints
+    param_options = {}
+
+    for para_name, para_field in params_schema.items():
+        default = para_field.get("default", None)
+        para_desc = para_field.get("description", "")
+        
+        if para_name in para_range_override:
+            para_range = para_range_override[para_name]
+        else:  # Get from pydantic schema
+            para_range = [-20, 20]  # Default range
+            # Override the range if specified
+            if "minimum" in para_field:
+                para_range[0] = para_field["minimum"]
+            if "maximum" in para_field:
+                para_range[1] = para_field["maximum"]
+            para_range = [type(default)(x) for x in para_range]
+            
+        param_options[para_name] = dict(
+            para_range=para_range,
+            para_default=default,
+            para_symbol=ParamsSymbols[para_name],
+            para_desc=para_desc,
+        )
+    return param_options
+
+def select_agent_args(agent_args_options):
     agent_args = {}
     # Select agent parameters
     for n, arg_name in enumerate(agent_args_options.keys()):
@@ -51,26 +88,50 @@ def get_arg_params(agent_args_options):
         )
     return agent_args
 
+def select_params(params_options):
+    params = {}
+    # Select agent parameters
+    for n, para_name in enumerate(params_options.keys()):
+        para_range = params_options[para_name]['para_range']
+        para_default = params_options[para_name]['para_default']
+        para_symbol = params_options[para_name]['para_symbol']
+        para_desc = params_options[para_name].get('para_desc', '')
+        
+        if para_range[0] == para_range[1]:  # Fixed parameter
+            params[para_name] = para_range[0]
+            st.markdown(f"{para_symbol} ({para_name}) is fixed at {para_range[0]}")
+        else:
+            params[para_name] = st.slider(
+                f"{para_symbol} ({para_name})", 
+                para_range[0], para_range[1], 
+                para_default
+            )
+    return params
 
 def app():
     
-    # A dropdown to select model family
+    # -- Select agent family --
     model_family = st.selectbox("Select model family", list(model_families.keys()))
     
+    # -- Select agent --
     agent_class = getattr(generative_model, model_families[model_family])
-    agent_args_options = _get_agent_args(agent_class)
-    agent_args = get_arg_params(agent_args_options)
+    agent_args_options = _get_agent_args_options(agent_class)
+    agent_args = select_agent_args(agent_args_options)
     
-    # Initialize the model
-    forager_collection = ForagerCollection()
-    all_presets = forager_collection.FORAGER_PRESETS.keys()
-    
-    forager = forager_collection.get_preset_forager("Hattori2019", seed=42)
+    # -- Select agent parameters --
+    # Initialize the agent
+    forager = agent_class(**agent_args)
+    # Get params options
+    params_options = _get_params_options(forager.ParamModel)
+    params = select_params(params_options)
     
     forager.set_params(
         softmax_inverse_temperature=5,
         biasL=0,
     )
+
+    # forager_collection = ForagerCollection()
+    # all_presets = forager_collection.FORAGER_PRESETS.keys()
 
     # Create the task environment
     task = CoupledBlockTask(reward_baiting=True, num_trials=1000, seed=42) 
