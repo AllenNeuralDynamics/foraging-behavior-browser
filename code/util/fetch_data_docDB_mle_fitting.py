97f3dc423c7d832ec2a3f4d750b0e9dcf9a3531c
houhan@gmail.com <han.hou@alleninstitute.org>
2024-12-11 01:35:09 +0000

    improve fetch_mle_fitting_results

--- code/util/fetch_data_docDB_mle_fitting.py
+++ code/util/fetch_data_docDB_mle_fitting.py
@@ -11,6 +11,13 @@ import numpy as np
 
 from aind_data_access_api.document_db import MetadataDbClient
 
+FORAGER_PRESET = {
+    "Win-Stay-Lose-Shift": "WSLS",
+    "Rescorla-Wagner": "QLearning_L1F0_epsi",
+    "Bari2019": "QLearning_L1F1_CK1_softmax",
+    "Hattori2019": "QLearning_L2F1_softmax",
+}
+
 @st.cache_resource
 def load_client():
     return MetadataDbClient(
@@ -21,40 +28,70 @@ def load_client():
     
 client = load_client()
 
-def fetch_mle_fitting_results():
+@st.cache_data(ttl=3600*12) # Cache the df_docDB up to 12 hours
+def fetch_mle_fitting_results(model_alias="Hattori2019"):
     """Fetch mle fitting results from docDB
     """
 
-    import time 
-    start_time = time.time()
-    
-    results = client.retrieve_docdb_records(
-        filter_query={
-            "analysis_results.fit_settings.agent_alias": "QLearning_L1F1_CK1_softmax",
-        },
-        projection={"nwb_name": 1, "analysis_results.params": 1},
-    )
-    print(f"Time elapsed: {time.time() - start_time:.2f} seconds")
-    
-    start_time = time.time()
-    
+    # --- Fetch MLE fitting results ---
     pipeline = [
         {
             "$match": {
-                "analysis_results.fit_settings.agent_alias": "QLearning_L1F1_CK1_softmax"
+                "analysis_results.fit_settings.agent_alias": FORAGER_PRESET[model_alias],
             }
         },
         {
             "$project": {
                 "_id": 0,
                 "nwb_name": 1,
-                "params": "$analysis_results.params",
+                f"{model_alias}": "$analysis_results.params",
             }
         },
     ]
-    
     records = client.aggregate_docdb_records(pipeline=pipeline)
+    df = pd.json_normalize(records)
     
-    print(f"Time elapsed: {time.time() - start_time:.2f} seconds")
-    
-    
+
+    # Apply the function to create new columns
+    df[["subject_id", "session_date", "nwb_suffix"]] = df["nwb_name"].apply(
+        lambda x: pd.Series(split_nwb_name(x))
+    )
+
+    return df
+
+
+# --- Helper functions ---
+
+# Function to split the `nwb_name` column
+def split_nwb_name(nwb_name):
+    """Turn the nwb_name into subject_id, session_date, nwb_suffix in order to be merged to
+    the main df.
+
+    Parameters
+    ----------
+    nwb_name : str. The name of the nwb file, e.g.
+        "721403_2024-08-09_08-39-12.nwb"
+        "685641_2023-10-04.nwb",
+        ...
+
+    Returns
+    -------
+    subject_id : str. The subject ID
+    session_date : str. The session date
+    nwb_suffix : int. The nwb suffix
+    """
+    parts = nwb_name.replace(".nwb", "").split("_")
+    subject_id = parts[0]
+    session_date = parts[1]
+    try:
+        nwb_suffix = int(parts[2].replace("-", "")) if len(parts) > 2 else 0
+    except:
+        nwb_suffix = 0
+    return subject_id, session_date, nwb_suffix
+
+
+
+
+df = fetch_mle_fitting_results(model_alias="Hattori2019")
+st.write(df)
+
